2ac26359d0507dfe4e0ca202cfca82447b612f2a
Tiegan Benson <tiegben@gmail.com>
2025-12-01 21:09:24 -0700

    fixed calendar styles

--- assets/src/js/calendar.js
+++ assets/src/js/calendar.js
@@ -1,8 +1,11 @@
 /* Style children of the TEC calendar iframe */
-document.getElementById('tec-calendar-main').onload = function() {
-    var iframeDoc = this.contentWindow.document;
-    var element = iframeDoc.querySelector('html');
-    if (element) {
-        element.style.backgroundImage = 'none';
-    }
-};
+const body = document.querySelector( "body" );
+if ( body.classList.contains( "page-id-316" ) ) {
+  document.getElementById('tec-calendar-main').onload = function() {
+      var iframeDoc = this.contentWindow.document;
+      var element = iframeDoc.querySelector('html');
+      if (element) {
+          element.style.backgroundImage = 'none';
+      }
+  };
+}
